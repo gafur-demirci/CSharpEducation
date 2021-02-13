8ce4cc278ffd22ce6d740792dfeb661e47cb1850
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-02-13 22:16:56 +0300

    BTK C# Kursu /w Engin Demiroğ

---
+++ Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    class Customer
+    {
+
+        // Field (Alan)
+        // public string FirstName;
+
+        // Property (Özellik)
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        //private string _firstName;
+        //public string FirstName { 
+        //    get { return "Mr. " + _firstName; }
+        //    set { _firstName = value; }
+        //}
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
