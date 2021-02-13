b005dd8a6120ae78ebf1061d7bccab5ed9a5860b
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-02-13 22:15:27 +0300

    Add project files.

---
+++ CSharpEducation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpEducation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
