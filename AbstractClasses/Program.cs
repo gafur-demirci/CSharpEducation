94fce5b255bbe4fa0f3b77c181d1571ce7d6ec12
gafur-demirci <73399810+gafur-demirci@users.noreply.github.com>
2021-02-14 19:35:15 +0300

    BTK C# Kursu /w Engin Demiroğ

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Database database = new Database();
+
+            Database database = new SqlServer();
+            database.Add();
+            database.Delete();
+            Database database1 = new Oracle();
+            database1.Add();
+            database1.Delete();
+
+        }
+    }
+    // İnterface'ler de olduğu gibi abstract class'lar da new lenemezler.
+    // Class'lar inheritance olarak sadece bir tane abstract class alabilir ama birden fazla interface alabilir.
+    abstract class Database
+    {
+        // Tamamlanmış method
+        // Her database için Add methodu aynıdır.
+        public void Add()
+        {
+            Console.WriteLine("Added by default");
+        }
+        // Tamamlanmamış method
+        // Database lerde farklı delete methodları olsun demek.
+        public abstract void Delete();
+    }
+
+    class SqlServer : Database                         // SqlServer , database i inheritance edebilir bu sayade sadece delete methodu gelir çünkü abstract  method aynı zamanda virtual olarak 
+                                                       // davrandığı içinden implemente ederken override olarak gelir.
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by SqlServer");
+        }
+    }
+
+    class Oracle : Database                            // Oracle , database i inheritance edebilir bu sayade sadece delete methodu gelir çünkü abstract  method aynı zamanda virtual olarak 
+                                                       // davrandığı içinden implemente ederken override olarak gelir.
+    {
+        public override void Delete()
+        {
+            Console.WriteLine("Deleted by Oracle");
+        }
+    }
+}
---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.Logger = new DatabaseLogger();
+            customerManager.Logger = new SmsLogger();                // Bu şekilde gibi gibi.
+            customerManager.Add();
+            
+        }
+    }
+    // Bu sayade yeni gelen Log lama türünde sadece class ı kopyalayıp new olarak isimini değiştirmek yeterli olacaktır.
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added.");
+        }
+    }
+
+    // İnterface kullanma sebebimiz Database,File ve Sms için loglama kodları farklı olması.
+    interface ILogger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to File!");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms!");
+        }
+    }
+
+
+}
